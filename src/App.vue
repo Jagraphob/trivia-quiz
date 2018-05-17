1c610dd966f8afb76d83a30385488e16e35f56ad
Jagraphob Jantakananuruk <toe15g@gmail.com>
2018-05-17 15:47:27 +1200

    fix firebase import

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@
 </template>
 
 <script>
-import firebase from 'firebase'
+import firebase from 'firebase/app'
 
 export default {
   name: 'app',
--- src/main.js
+++ src/main.js
@@ -5,7 +5,10 @@ import store from './store'
 import './registerServiceWorker'
 import Vuetify from 'vuetify'
 import moment from 'moment'
-import firebase from 'firebase'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/firestore'
 
 Vue.config.productionTip = false
 
