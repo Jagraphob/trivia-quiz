c41f8de47cfccf3e71f517e45093dc97dd2c3c29
Jagraphob Jantakananuruk <toe15g@gmail.com>
2018-05-18 11:03:01 +1200

    answer selection

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -4,10 +4,10 @@
     <v-card>
       <v-container grid-list-lg>
         <v-layout row wrap>
-          <v-flex x12>
-            <v-card v-for="question in questions" :key="question.question">
-              <h3>Question : {{ question.question}}</h3>
-              <v-radio-group>
+          <v-flex xs12 v-for="question in questions" :key="question.question">
+            <v-card color="teal lighten-4">
+              <h3 class="headline">Question : {{ question.question}}</h3>
+              <v-radio-group v-model="answers[question.Id]">
                 <v-radio v-for="ans in question.answers" :key="ans" :label="ans" :value="ans"></v-radio>
               </v-radio-group>
             </v-card>
@@ -15,7 +15,11 @@
         </v-layout>
       </v-container>
     </v-card>
-    {{questions}}
+    <div class="row">
+      <v-btn large color="success">Submit</v-btn>
+      <v-btn large color="warning" @click="reset">Reset</v-btn>
+    </div>
+  {{ answers }}
   </div>
 </template>
 
@@ -27,13 +31,14 @@ export default {
   data () {
     return {
       questions: {},
-      error: ""
+      error: "",
+      answers: []
     }
   },
   created () {
     axios.get('https://opentdb.com/api.php?amount=10&type=multiple')
       .then((response) => {
-        var count = 0
+        var count = -1
         var mappedQ = response.data.results.map((q) => {
           var ans = q.incorrect_answers
           ans.push(q.correct_answer)
@@ -57,6 +62,9 @@ export default {
       })
   },
   methods: {
+    reset () {
+      this.answers = []
+    },
     shuffle (a) {
       for (let i = a.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
